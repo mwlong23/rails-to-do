b429cb8d7d936f332e91894d954319b9ada368e0
Gennaro Anzalone and Mitchell Long <gennaro+mitchell@>
2017-11-13 10:57:59 -0800

    add delete list function

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -37,6 +37,11 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
 
   private
   def list_params
