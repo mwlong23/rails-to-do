179598df6eec2a454530878dd2748185a108fa6c
Gennaro Anzalone and Mitchell Long <gennaro+mitchell@>
2017-11-13 13:56:53 -0800

    add title tags to views and flash notifications

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,6 @@ class TasksController < ApplicationController
   def edit
     @list = List.find(params[:list_id])
     @task = @list.tasks.find(params[:id])
-    render :edit
   end
 
   def update
