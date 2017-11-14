d7dd1c680929c004820720f02041ae95102e502f
Gennaro Anzalone and Mitchell Long <gennaro+mitchell@>
2017-11-13 16:25:33 -0800

    add not done/done scope to task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,7 @@ class TasksController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @task = @list.tasks.new(task_params)
+    @task.done = false
     if @task.save
       redirect_to list_path(@task.list)
     else
@@ -36,12 +37,18 @@ class TasksController < ApplicationController
     redirect_to list_path(@task.list)
   end
 
+  def done
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    @task.done = true
+    redirect_to list_path(@task.list)
+  end
 
 
 
   private
     def task_params
-      params.require(:task).permit(:description)
+      params.require(:task).permit(:description, :done)
     end
 
 end
